2d7b9b195c83fc2d167baf4c5d7e54bc8feef0ac
Dilshan Hangawaththa <dilshanhangawaththa@gmail.com>
2021-03-15 15:16:14 +0530

    Changed bucket name

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -4,7 +4,7 @@ terraform {
     region  = "us-east-1"
     profile = "default"
     key     = "terraformstatefile"
-    bucket  = "terraformstatebucket-cd-demo"
+    bucket  = "ck-dev-test-candidate-dilshan"
   }
 
 }
